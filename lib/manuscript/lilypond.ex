c34efa0910ae1dcc42dc3089cd31b49ec561086e
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-06-02 13:02:59 -0400

    Refactor to template, multiple systems where appropriate (#2)

--- lib/manuscript/lilypond.ex
+++ lib/manuscript/lilypond.ex
@@ -5,75 +5,68 @@ defmodule Manuscript.Lilypond do
   [[version] | _] = Regex.scan(~r/[\d\.]+/, lilypond_version)
   @lilypond_version version
 
+  @priv :code.priv_dir(:manuscript)
+  File.mkdir_p(@priv)
+
   def generate_lilypond(staves) do
     content = write_lilypond(staves)
 
-    :ok = File.write("priv/score.ly", content)
-    {"", 0} = System.cmd("lilypond", ["-s", "--png", "--o", "priv/score", "priv/score.ly"])
-    {:ok, image_data} = File.read("priv/score.png")
+    path = Path.join(@priv, "score-for-png")
+    ly = path <> ".ly"
+    png = path <> ".png"
+    :ok = File.write(ly, content)
+    {"", 0} = System.cmd("lilypond", ["-s", "--png", "--o", path, ly])
+    {:ok, image_data} = File.read(png)
     data = "data:image/png;base64,#{Base.encode64(image_data)}"
-    :ok = File.rm("priv/score.png")
+    :ok = File.rm(png)
     data
   end
 
   def generate_pdf_data(staves) do
     content = write_lilypond(staves)
 
-    :ok = File.write("priv/score.ly", content)
-    {"", 0} = System.cmd("lilypond", ["-s", "--o", "priv/score", "priv/score.ly"])
-    {:ok, pdf_data} = File.read("priv/score.pdf")
+    path = Path.join(@priv, "score-for-pdf")
+    ly = path <> ".ly"
+    pdf = path <> ".pdf"
+
+    :ok = File.write(ly, content)
+    {"", 0} = System.cmd("lilypond", ["-s", "--o", path, ly])
+    {:ok, pdf_data} = File.read(pdf)
     data = "data:application/pdf;base64,#{Base.encode64(pdf_data)}"
-    :ok = File.rm("priv/score.pdf")
+    :ok = File.rm(pdf)
     data
   end
 
   def write_lilypond(staves) do
-    groups =
-      Enum.chunk_by(staves, & &1.template.family)
-      |> Enum.map_join("\n", fn family ->
-        case family do
-          [] ->
-            ""
+    File.read!(Path.join(:code.priv_dir(:manuscript), "lilypond/score_template.ly"))
+    |> String.replace("__LILYPOND_VERSION__", @lilypond_version)
+    |> String.replace("__GROUPS__", staff_groups(staves))
+  end
 
-          instruments ->
-            """
-            \\new StaffGroup <<
-              #{Enum.map_join(instruments, "\n", &Instrument.staff/1)}
-            >>
-            """
-        end
-      end)
+  defp staff_groups(staves) do
+    staff_count = staves |> Enum.map(&Instrument.Template.staff_count(&1.template)) |> Enum.sum()
 
-    """
-    \\version "#{@lilypond_version}"
+    measures =
+      case staff_count do
+        n when n < 4 -> "s1 \\break s1 \\break s1 \\break s1"
+        n when n < 6 -> "s1 \\break s1 \\break s1"
+        n when n < 9 -> "s1 \\break s1"
+        _ -> "s1"
+      end
 
+    Enum.chunk_by(staves, & &1.template.family)
+    |> Enum.map_join("\n", fn family ->
+      case family do
+        [] ->
+          ""
 
-    #(set-default-paper-size "tabloidlandscape")
-
-    \\paper {
-      ragged-right = ##f
-      ragged-last-right = ##f
-      ragged-last-bottom = ##f
-      ragged-bottom = ##f
-      tagline = ##f
-      padding = 0
-      right-margin = .5\\in
-      left-margin = .5\\in
-    }
-
-    \\score {
-      <<
-      #{groups}
-      >>
-    }
-
-    \\layout {
-      \\context {
-        \\Score
-        \\omit TimeSignature
-
-      }
-    }
-    """
+        instruments ->
+          """
+          \\new StaffGroup <<
+            #{Enum.map_join(instruments, "\n", &Instrument.staff(&1, measures))}
+          >>
+          """
+      end
+    end)
   end
 end
